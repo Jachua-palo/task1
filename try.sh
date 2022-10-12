86508760f1b4b5d98a96e6e0ef71d2896766f3ca
Jachua-palo <61770406+Jachua-palo@users.noreply.github.com>
2022-10-12 14:44:40 +0800

    add comments

--- try.sh
+++ try.sh
@@ -13,8 +13,10 @@ fi
 time="18/May/2015:20"
 logfile=$1
 
+#filter logs only for last hour 
 errorLogs=$(awk -v date="$time" '($4 ~ date && $9 ~ /[4-5][0-9][0-9]/)' $logfile)
 
+#check if it crosses treshold of 100 5xx and 4xx and send email
 if [ $(echo $errorLogs|  wc -l) -gt 100 ]
 then
    body=$( echo $errorLogs | awk '{print $9,$7}' | uniq -c | sort -r)	
